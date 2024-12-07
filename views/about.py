a867c28787c3287f4cbf51e787224ee49ab3d87c
kahosadi <kahosadi@gmail.com>
2024-12-07 17:45:54 -0600

    Create about.py

---
+++ views/about.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("About")
+
+st.markdown("This web app is built as a project for INFO 5307.021 Knowledge Management Tools and Technologies (Fall 2024)")
+st.markdown("The purpose of this web app is to assess various metadata elements and enhance their quality by identifying and noting necessary improvements.")
