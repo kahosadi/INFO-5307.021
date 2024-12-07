0638cca6df877f3d52e7fba0dbb5ac14a17c4f2a
kahosadi <kahosadi@gmail.com>
2024-12-07 17:39:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,33 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.set_page_config(
+    page_title = "Metadata Evaluator v0.1"
+)
 
-st.write('Hello world!')
+#st.title("Metadata Evaluator v0.1")
+
+#sidebar
+#with st.sidebar:
+#    st.header("Menu")
+#    st.subheader("Menu")
+   
+#page setup
+main_page = st.Page(
+    page = "views/main.py",
+    title = "Main",
+    default = True
+)
+
+authority_page = st.Page(
+    page = "views/authority.py",
+    title = "Name Headings"
+)    
+
+about_page = st.Page(
+    page = "views/about.py",
+    title = "About"
+)    
+
+pg = st.navigation(pages=[main_page, authority_page, about_page])
+
+pg.run()
